0947c5074fe09af4243aaa18b88b5cf2e9253c86
Oleksndr <a.popovych.89@gmail.com>
2022-04-13 15:10:02 +0300

    Fixed errors.

--- src/lesson13/practic/Task.java
+++ src/lesson13/practic/Task.java
@@ -1,4 +1,6 @@
 /**
+ * Practical task:
+ * <p>
  * In the next method calculate amount of characters in the string. Need to find logic errors
  */
 package lesson13.practic;
@@ -14,17 +16,14 @@ public class Task {
         String character = "";
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Input word");
-        try {
-            testWord = br.readLine();
-            System.out.println("Input character");
-            character = br.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        testWord = br.readLine();
+        System.out.println("Input character");
+        character = br.readLine();
         for (int i = 0; i < testWord.length(); i++) {
             character = testWord.substring(1, 1);
             if (character.equals(character)) countCharacters++;
         }
         System.out.println("There are character" + character + " " + countCharacters + " times in the word " + testWord);
+
     }
 }
